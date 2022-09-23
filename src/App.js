569d482fdfee0524cdd07d5b79be7ed68ba5d13d
George Santos <george.santos_ufmg@outlook.com>
2022-09-23 19:58:19 -0300

    fix: Consertar packageLock

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import RecipiesProvider from './context/RecipiesProvider';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE 2.0</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <RecipiesProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          {/* <Route path='/meals' component={ } />
+          <Route path='/drinks' component={ } />
+          <Route path='/meals/:id' component={ } />
+          <Route path='/drinks/:id' component={ } />
+          <Route path='/meals/:id/in-progress' component={ } />
+          <Route path='/drinks/:id/in-progress' component={ } />
+          <Route path='/profile' component={ } />
+          <Route path='/done-recipes' component={ } />
+          <Route path='/favorite-recipes' component={ } /> */}
+        </Switch>
+      </BrowserRouter>
+    </RecipiesProvider>
+
   );
 }
 

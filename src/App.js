fb80b9066c7f77afe87f58d2206d40a54891299b
George Santos <george.santos_ufmg@outlook.com>
2022-09-24 00:02:16 -0300

    refactor: Mudar BrowserRouter para index.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import RecipiesProvider from './context/RecipiesProvider';
 import Login from './pages/Login';
+import Recipes from './pages/Recipes';
 
 function App() {
   return (
     <RecipiesProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ Login } />
-          {/* <Route path='/meals' component={ } />
-          <Route path='/drinks' component={ } />
-          <Route path='/meals/:id' component={ } />
-          <Route path='/drinks/:id' component={ } />
-          <Route path='/meals/:id/in-progress' component={ } />
-          <Route path='/drinks/:id/in-progress' component={ } />
-          <Route path='/profile' component={ } />
-          <Route path='/done-recipes' component={ } />
-          <Route path='/favorite-recipes' component={ } /> */}
-        </Switch>
-      </BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/meals" component={ Recipes } />
+        {/* <Route path='/drinks' component={ } />
+        <Route path='/meals/:id' component={  } />
+        <Route path='/drinks/:id' component={ } />
+        <Route path='/meals/:id/in-progress' component={ } />
+        <Route path='/drinks/:id/in-progress' component={ } />
+        <Route path='/profile' component={ } />
+        <Route path='/done-recipes' component={ } />
+        <Route path='/favorite-recipes' component={ } /> */}
+      </Switch>
     </RecipiesProvider>
-
   );
 }
 

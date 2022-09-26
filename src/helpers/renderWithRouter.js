1db2c28d6a9712e328c8679d68eb12ccd3bc14bb
George Santos <george.santos_ufmg@outlook.com>
2022-09-25 23:32:13 -0300

    fix: fix renderWithRouter

--- src/helpers/renderWithRouter.js
+++ src/helpers/renderWithRouter.js
@@ -5,8 +5,10 @@ import { render } from '@testing-library/react';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
+
   return ({
-    ...render(<Router history={ history }>{component}</Router>, history),
+    ...render(<Router history={ history }>{component}</Router>),
+    history,
   });
 };
 

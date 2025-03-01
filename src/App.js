2ab41029703b94dff50f579d5be0aaf40546beab
rafael <rafaelbercosta@gmail.com>
2025-02-28 21:19:33 -0300

    asterisco

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ function App() {
         <Route exact path="/profile" component={ Profile } />
         <Route exact path="/done-recipes" component={ DoneRecipes } />
         <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route path="*" component={ Login } />
       </Switch>
     </RecipesProvider>
   );
